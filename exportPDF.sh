5eb0eb5d569c4a4e8836992bf819d4116a469931
iamwan <83023391+iamwan@users.noreply.github.com>
2022-08-17 09:11:13 +0200

    Update exportPDF.sh
    - Commented out the diagnostic step
    - Replaced absolute paths for relatives

--- exportPDF.sh
+++ exportPDF.sh
@@ -5,14 +5,16 @@ docker stop dita_container > /dev/null 2>&1
 docker rm dita_container -f > /dev/null 2>&1
 docker rmi dita -f > /dev/null 2>&1
 
-echo "\n\nBuildind the dita image...\n\n"
+echo "\n\nBuilding the dita image...\n\n"
 
 docker build --no-cache -t dita . 
 
 echo "\n\nExporting HTML ...\n\n"
-docker run --name dita_container -it -v /Users/owenrowe/git/dita-demo-setup:/src dita -p /src/html.yaml -o /src/out -f html5 -v && \
-wait && \
-docker exec -ti dita_container /bin/bash -c "cd /src/out && zip -r /src/out.zip ."
+docker run --name dita_container -it -v `pwd`:/src dita -p /src/html.yaml -o /src/out -f html5 -v && \
+wait 
+
+# && \
+# docker exec -ti dita_container /bin/bash -c "cd /src/out && zip -r /src/out.zip ."
 
 echo "\n\nExporting PDF ...\n\n"
-docker run --rm -v /Users/owenrowe/git/dita-demo-setup/out:/src minidocks/weasyprint /src/out/document.html /src/out/success.pdf
+docker run --rm -v `pwd`:/src minidocks/weasyprint /src/out/document.html /src/out/success.pdf
